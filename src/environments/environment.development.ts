62ae0bcabb4040cb61c8d5c0f4d64181999c45c8
spalax-dev <jorgity345@gmail.com>
2026-07-11 00:02:29 -0500

    feat: replace AROS logo with project logo, update icons, redesign login page

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api',
   wsUrl: 'http://localhost:8080/ws',
-  customer: 'RMS',
+  customer: 'Restaurante de prueba',
   baseColor: '#FABD2F',
   agentHost: 'http://localhost:8000/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.rms.aros.services/api',
   wsUrl: 'https://api.rms.aros.services/ws',
-  customer: 'RMS',
+  customer: 'Restaurante de prueba',
   baseColor: '#F9BB0B',
   agentHost: 'https://abi.aros.services/api/v1'
 };
